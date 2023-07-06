e1e8945502dfea9e9ab342058b72440bd7eebd21
Arvind Yadav <arvind14nitt@gmail.com>
2023-07-06 09:07:51 +0530

    Fixed values

--- my-app/src/ExpenseItem.js
+++ my-app/src/ExpenseItem.js
@@ -1,11 +1,14 @@
 import './ExpenseItem.css';
 function ExpenseItem()
 {
-     return (
+    const date = "22-May-2022";
+    const description = "Purchase a watch";
+    const price = 20;
+      return (
 	<div className="expense-item">
-	<div className="expense-item__date"> 22-May-2022     </div>
-	<div className="expense-item__description"><h2> Purchase a watch</h2>     </div>
-	<div className="expense-item__price"> $20     </div>
+	<div className="expense-item__date"> {date}    </div>
+	<div className="expense-item__description"><h2> {description} </h2>     </div>
+	<div className="expense-item__price"> ${price}     </div>
 	</div>
 	);
 }
