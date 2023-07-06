9d70ef4fc14775330c70e160fcff8578cefcc5ae
Arvind Yadav <arvind14nitt@gmail.com>
2023-07-06 09:02:57 +0530

    Hardcoded data

--- my-app/src/ExpenseItem.js
+++ my-app/src/ExpenseItem.js
@@ -3,9 +3,9 @@ function ExpenseItem()
 {
      return (
 	<div className="expense-item">
-	<div className="expense-item__date"> Date     </div>
-	<div className="expense-item__description"><h2> Description</h2>     </div>
-	<div className="expense-item__price"> Price     </div>
+	<div className="expense-item__date"> 22-May-2022     </div>
+	<div className="expense-item__description"><h2> Purchase a watch</h2>     </div>
+	<div className="expense-item__price"> $20     </div>
 	</div>
 	);
 }
